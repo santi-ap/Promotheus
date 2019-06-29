180cb3e73c482e872ea18b16a9a4d8e980779777
Dacord <dacord97@hotmail.com>
2019-06-28 20:13:37 -0600

    + Usuario class + Controller, LoginController & RegisterController

---
+++ Promotheus/src/java/com/bsod/promotheus/controladores/LoginController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bsod.promotheus.controladores;
+
+import com.bsod.promotheus.usuario.Usuario;
+
+/**
+ *
+ * @author DacordMachine
+ */
+public class LoginController extends Controller {
+    
+    private Usuario Logear(){
+        
+        return new Usuario();
+    }
+    
+}
