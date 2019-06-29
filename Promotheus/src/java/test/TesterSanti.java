8887328ba4002fadb6ba7337bf2399bb53287269
Dacord <dacord97@hotmail.com>
2019-06-29 11:11:36 -0600

    +Message display in Log in

--- Promotheus/src/java/test/TesterSanti.java
+++ Promotheus/src/java/test/TesterSanti.java
@@ -20,11 +20,11 @@ public class TesterSanti {
     public static void main(String[] args) {
         ServicioUsuario su = new ServicioUsuario();
 //        System.out.println(su.selectAll("nombreUsuario", "user1"));
-        Usuario u = new Usuario("user5","5@5","pass5");
+        Usuario u = new Usuario("user8","8@8","pass8");
         su.insert(u);
 //        su.update("nombreUsuario", "Santi", "correoUsuario", "2@2");
 //        System.out.println(su.select("nombreUsuario", "correoUsuario", "2@2"));
-        su.delete("correoUsuario", "2@2");
+        //su.delete("correoUsuario", "2@2");
     }
     
 }
