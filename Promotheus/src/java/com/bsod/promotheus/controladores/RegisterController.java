0782d30374389ef82b0e15bcf18c8891d6c0816c
David Alfonso <code.david99@hotmail.com>
2019-06-29 12:12:18 -0600

    se creo en TegisterController el metodo par insertar el usuario a la BD

--- Promotheus/src/java/com/bsod/promotheus/controladores/RegisterController.java
+++ Promotheus/src/java/com/bsod/promotheus/controladores/RegisterController.java
@@ -4,8 +4,11 @@
  * and open the template in the editor.
  */
 package com.bsod.promotheus.controladores;
+import com.bsod.promotheus.servicios.ServicioUsuario;
 import com.bsod.promotheus.usuario.Usuario;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import static org.primefaces.component.focus.Focus.PropertyKeys.context;
 
 /**
  *
@@ -13,15 +16,51 @@ import javax.faces.application.FacesMessage;
  */
 public class RegisterController extends Controller {
     
-    
+    private String nombreInput;
+    private String passDosInput;
+
+    public String getNombreInput() {
+        return nombreInput;
+    }
+
+    public void setNombreInput(String nombreInput) {
+        this.nombreInput = nombreInput;
+    }
+
+    public String getPassDosInput() {
+        return passDosInput;
+    }
+
+    public void setPassDosInput(String passDosInput) {
+        this.passDosInput = passDosInput;
+    }
 
     public RegisterController(String correoInput, String passInput) {
         super(correoInput, passInput);
     }
+    
+    public RegisterController(){}
+
+    
 
     
 
     public Usuario registrar() {
+        ServicioUsuario su = new ServicioUsuario();
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        if (super.existeCorreo(this.getCorreoInput())){
+          //  context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "El correo ya se ha usado", "Utilizar otro correo"));
+            System.out.println("Ya exsite");
+        }else{
+            Usuario u = new Usuario();
+            u.setNombreUsuario(this.getNombreInput());
+            u.setCorreoUsuario(this.getCorreoInput());
+            u.setPassUsuario(this.getPassInput());
+            su.insert(u);
+            System.out.println("Usuario insertado a BD");
+        }
 
         return new Usuario();
     }
