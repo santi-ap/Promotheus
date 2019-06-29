a461b7a5117c71fdab4de7ea288b5042d3df9d87
Santiago <santialfonso@10.18.14.125>
2019-06-28 20:57:23 -0600

    agregue InterfaceDAO y CRUD del ServicioUsuario

--- Promotheus/src/java/com/bsod/promotheus/servicios/Servicio.java
+++ Promotheus/src/java/com/bsod/promotheus/servicios/Servicio.java
@@ -6,6 +6,8 @@
 package com.bsod.promotheus.servicios;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -13,18 +15,31 @@ import java.sql.Connection;
  */
 public abstract class Servicio {
     
-    private String DBC_DRIVE = "com.mysql.jdbc.Driver";
-    private String URL = "jdbc:mysql://ulatina3.ceuol7o4t0su.us-east-2.rds.amazonaws.com:3306/AWS Test 1";
+    private String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private String DB_URL = "jdbc:mysql://ulatina3.ceuol7o4t0su.us-east-2.rds.amazonaws.com:3306/AWS Test 1";
     private String USER = "promouser";
     private String PASS = "123456";
     private Connection conn = null;
     
-    private void conectar(){
-        
+    protected void conectar(){
+        try{
+            //STEP 1: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+            //STEP 2: Open a connecNon
+            System.out.println("Connecting to database...");
+            conn=DriverManager.getConnection(DB_URL,USER,PASS);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
     
-    private void desconectar(){
-        
+    protected void desconectar() throws SQLException{
+        try{
+            if(!conn.isClosed()){
+                conn.close();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
-    
 }
